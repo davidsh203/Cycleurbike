dad03b224049cee55c2ac5048202816114c4cb34
David <davidsh203@gmail.com>
2020-09-09 15:15:11 +0300

    upload again all commit that deleted
    fix problems with code in MainAppPage

---
+++ app/src/main/java/com/example/cycleurbike/youtube/ApiKey.java
@@ -0,0 +1,8 @@
+package com.example.cycleurbike.youtube;
+
+public class ApiKey {
+
+    //youtube developer API Key
+    public static String DEVELOPER_KEY = "AIzaSyD7Y9zKeTjuLMKlw-AKvOZNCqUeBRNp4Uc";
+}
+
